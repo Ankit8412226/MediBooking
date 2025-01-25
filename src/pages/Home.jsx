e337d3b414d53aaada3b05dae57686d141eada81
Ankit8412226 <125825310+Ankit8412226@users.noreply.github.com>
2025-01-26 01:18:18 +0530

    BannerDone

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../components/header";
 import Hero from "../components/hero";
 import SpecialityMenu from "../components/SpecialityMenu";
 import TopDoctors from "../components/TopDoctors";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <SpecialityMenu />
       <TopDoctors />
+      <Banner />
     </div>
   );
 };
